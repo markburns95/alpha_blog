3247934109348a9cba7233bbd2ab2bb3d9f6ff44
mark <markburns95@gmail.com>
2020-04-05 19:47:36 -0600

    Added signup for users

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   resources :articles # , only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  # Next 3 lines show how to reroute to a model
+  get 'signup', to: 'users#new'
+  # post 'users', to: 'users#create' # or
+  resources :users, except: [:new]
 end
