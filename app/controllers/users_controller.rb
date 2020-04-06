7fb896aaa2dbb5188306c1e01b3ae78b6391174f
mark <markburns95@gmail.com>
2020-04-05 20:05:29 -0600

    Add user edit and partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
   def new
     @user = User.new
   end
@@ -11,9 +12,20 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  def edit
+  end
+  def update
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfully!"
+      redirect_to articles_path
+    end
+  end
 
   private
 
+  def set_user
+    @user = User.find(params[:id])
+  end
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
